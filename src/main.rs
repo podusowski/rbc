1e1c7b2e3f4476d959a3fac8f84767ca6106de34
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-02-05 20:01:25 +0100

    getting seed nodes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    println!("looking for seed nodes");
+    for addr in tokio::net::lookup_host("seed.bitcoin.sipa.be:0").await.unwrap() {
+        println!("{:?}", addr);
+    }
 }
