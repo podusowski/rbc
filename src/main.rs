b1fba1456df35f4d2878246b07c15444c01e624e
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-02-05 20:29:05 +0100

    connect to node

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::net::SocketAddr;
+
+async fn node(addr: SocketAddr) {
+    println!("connecting to {}", addr);
+    let mut sock = tokio::net::TcpSocket::new_v4().unwrap();
+    sock.connect(addr).await.unwrap();
+}
+
 #[tokio::main]
 async fn main() {
     println!("looking for seed nodes");
-    for addr in tokio::net::lookup_host("seed.bitcoin.sipa.be:0").await.unwrap() {
-        println!("{:?}", addr);
-    }
+    let nodes = tokio::net::lookup_host("seed.bitcoin.sipa.be:8333")
+        .await
+        .unwrap()
+        .filter(SocketAddr::is_ipv4)
+        .take(3)
+        .map(|addr| tokio::spawn(node(addr)));
+    futures::future::join_all(nodes).await;
 }
