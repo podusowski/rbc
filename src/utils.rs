e4157bbe860fbf5a7750689562ff8536b7a7baf1
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-02-06 13:52:55 +0100

    missing file

---
+++ src/utils.rs
@@ -0,0 +1,11 @@
+//const fn fill_bytes(content: &[u8], size: usize) -> &[u8; size] {
+//
+//}
+//
+//#[cfg(tests)]
+//mod tests {
+//	#[test]
+//	fn bytes_are_filled() {
+//		assert_eq!(b"\01\0" == )
+//	}
+//}
