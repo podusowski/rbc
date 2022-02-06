b213b30418d814946c08d5a0c302d51e67d9712c
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-02-06 22:51:19 +0100

    protocol mod

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,11 @@
-use std::{io::Write, net::SocketAddr};
+use std::{net::SocketAddr, io::Write};
 
-use sha2::Digest;
-use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tokio::io::AsyncReadExt;
 
+use crate::protocol::BitcoinMessage;
+
+mod protocol;
 mod utils;
-
-/// The Bitcoin message header contains length and checksum of the payload that
-/// follows. Because of that, it's not possible to encode it in a single pass
-/// and some intermediate form is needed.
-struct BitcoinMessage {
-    header: [u8; 4 + 12 + 4 + 4],
-    // TODO: Optimize this!
-    payload: Vec<u8>,
-}
-
-impl BitcoinMessage {
-    fn new() -> Self {
-        let mut message = Self {
-            header: Default::default(),
-            payload: Vec::new(),
-        };
-
-        let mut header = message.header.as_mut_slice();
-
-        // https://developer.bitcoin.org/reference/p2p_networking.html#message-headers
-
-        // Magic start string. Common for all messages.
-        header.write_all(&0xf9beb4d9u32.to_be_bytes()).unwrap();
-
-        // Command, or type of the message.
-        utils::write_padded_bytes(&mut header, b"version", 12);
-
-        // Length of the payload that follows the header. Set to zero, can be
-        // changed later.
-        header.write_all(&0u32.to_le_bytes()).unwrap();
-
-        // Checksum of the payload. Set to the empty string, can be changed
-        // later.
-        header.write_all(&0x5df6e0e2u32.to_le_bytes()).unwrap();
-
-        message
-    }
-
-    fn content_length(&mut self) -> &mut [u8] {
-        &mut self.header[16..20]
-    }
-
-    fn payload_hash(&mut self) -> &mut [u8] {
-        &mut self.header[20..24]
-    }
-
-    fn calculate_payload_hash(&self) -> [u8; 4] {
-        // First pass.
-        let mut hasher = sha2::Sha256::new();
-        hasher.update(&self.payload);
-        let first = hasher.finalize();
-
-        // Second pass.
-        let mut hasher = sha2::Sha256::new();
-        hasher.update(first);
-
-        let mut hash: [u8; 4] = Default::default();
-        hash.copy_from_slice(&hasher.finalize()[..4]);
-        hash
-    }
-
-    async fn write(mut self, sink: &mut (impl AsyncWrite + Unpin)) {
-        // Finalize the message.
-        let payload_len = self.payload.len() as u32;
-        self.content_length()
-            .copy_from_slice(&payload_len.to_le_bytes());
-
-        let hash = self.calculate_payload_hash();
-        self.payload_hash().copy_from_slice(&hash);
-
-        // Flush.
-        sink.write_all(&self.header).await.unwrap();
-        sink.write_all(&self.payload).await.unwrap();
-    }
-
-    fn payload_writer(&mut self) -> &mut impl Write {
-        &mut self.payload
-    }
-}
 
 async fn node(addr: SocketAddr) {
     // https://developer.bitcoin.org/devguide/p2p_network.html#connecting-to-peers
