6c0011ac94ca18752d7d3c5d7976669e654923a4
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-02-06 21:26:06 +0100

    version msg

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,82 @@ use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 mod utils;
 
-async fn write_padded_bytes<W>(sink: &mut W, buf: &[u8], total_length: usize)
-where
-    W: AsyncWrite + Unpin,
-{
-    sink.write_all(buf).await.unwrap();
+fn write_padded_bytes(sink: &mut impl Write, buf: &[u8], total_length: usize) {
+    sink.write_all(buf).unwrap();
     assert!(
         buf.len() <= total_length,
         "resulting write would be bigger than `total_length`"
     );
     for _ in 0..total_length - buf.len() {
-        sink.write(&[0]).await.unwrap();
+        sink.write(&[0]).unwrap();
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[tokio::test]
-    async fn smaller_data_gets_additional_padding() {
+
+    #[test]
+    fn smaller_data_gets_additional_padding() {
         let mut sink = Vec::new();
-        write_padded_bytes(&mut sink, &[1], 2).await;
+        write_padded_bytes(&mut sink, &[1], 2);
         assert_eq!(vec![1, 0], sink);
     }
 
-    #[tokio::test]
+    #[test]
     #[should_panic = "resulting write would be bigger than `total_length`"]
-    async fn panic_when_data_does_not_fit() {
+    fn panic_when_data_does_not_fit() {
         let mut sink = Vec::new();
-        write_padded_bytes(&mut sink, &[1, 2, 3], 2).await;
+        write_padded_bytes(&mut sink, &[1, 2, 3], 2);
         assert_eq!(vec![1, 0], sink);
+    }
+}
+
+struct BitcoinMessage {
+    header: [u8; 4 + 12 + 4 + 4],
+    // TODO: Optimize this!
+    payload: Vec<u8>,
+}
+
+impl BitcoinMessage {
+    fn new() -> Self {
+        let mut message = Self {
+            header: Default::default(),
+            payload: Vec::new(),
+        };
+
+        let mut header = message.header.as_mut_slice();
+
+        // https://developer.bitcoin.org/reference/p2p_networking.html#message-headers
+
+        // start string
+        header.write_all(&0xf9beb4d9u32.to_be_bytes()).unwrap();
+
+        // command
+        write_padded_bytes(&mut header, b"version", 12);
+
+        // content length
+        header.write_all(&0u32.to_le_bytes()).unwrap();
+        // checksum
+        header.write_all(&0x5df6e0e2u32.to_le_bytes()).unwrap();
+
+        message
+    }
+
+    fn content_length(&mut self) -> &mut [u8] {
+        &mut self.header[16..20]
+    }
+
+    async fn write(mut self, sink: &mut (impl AsyncWrite + Unpin)) {
+        let payload_len = self.payload.len() as u32;
+        self.content_length()
+            .copy_from_slice(&payload_len.to_le_bytes());
+        sink.write_all(&self.header).await.unwrap();
+        sink.write_all(&self.payload).await.unwrap();
+    }
+
+    fn payload_writer(&mut self) -> &mut impl Write {
+        &mut self.payload
     }
 }
 
@@ -43,63 +89,82 @@ async fn node(addr: SocketAddr) {
     let mut sock = tokio::net::TcpSocket::new_v4().unwrap();
     let mut stream = sock.connect(addr).await.unwrap();
 
-    // https://developer.bitcoin.org/reference/p2p_networking.html#message-headers
-    // start string
-    stream
-        .write_all(&0xf9beb4d9u32.to_le_bytes())
-        .await
-        .unwrap();
-    // command
-    write_padded_bytes(&mut stream, b"version", 12).await;
-    // content length
-    stream.write_all(&0u32.to_le_bytes()).await.unwrap();
-    // checksum
-    stream
-        .write_all(&0x5df6e0e2u32.to_le_bytes())
-        .await
-        .unwrap();
-
-    // version payload
+    let mut version = BitcoinMessage::new();
 
     // Protocol version. 70015 was the highest by the time this was written.
-    stream.write_all(&70015u32.to_le_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&70015u32.to_le_bytes())
+        .unwrap();
 
     // Services. We support none.
-    stream.write_all(&0u64.to_le_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&0u64.to_le_bytes())
+        .unwrap();
 
     // Current timestamp.
     let timestamp: u64 = std::time::SystemTime::now()
         .duration_since(std::time::SystemTime::UNIX_EPOCH)
         .unwrap()
         .as_secs();
-    stream.write_all(&timestamp.to_le_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&timestamp.to_le_bytes())
+        .unwrap();
 
     // Services of the other node. We support none.
-    stream.write_all(&0u64.to_le_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&0u64.to_le_bytes())
+        .unwrap();
 
     // "The IPv6 address of the receiving node as perceived by the transmitting node"
-    stream.write_all(&std::net::Ipv6Addr::LOCALHOST.octets()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&std::net::Ipv6Addr::LOCALHOST.octets())
+        .unwrap();
 
     // Port. Note the Big Endian.
-    stream.write_all(&0u16.to_be_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&0u16.to_be_bytes())
+        .unwrap();
 
     // Services. Again?
-    stream.write_all(&0u64.to_le_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&0u64.to_le_bytes())
+        .unwrap();
 
     // The IPv6 address of the transmitting node.
-    stream.write_all(&std::net::Ipv6Addr::LOCALHOST.octets()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&std::net::Ipv6Addr::LOCALHOST.octets())
+        .unwrap();
 
     // Port. Note the Big Endian.
-    stream.write_all(&0u16.to_be_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&0u16.to_be_bytes())
+        .unwrap();
 
     // Nonce. Not important for now.
-    stream.write_all(&0u64.to_le_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&0u64.to_le_bytes())
+        .unwrap();
 
     // Length of the user agent.
-    stream.write_all(&[0]).await.unwrap();
+    version.payload_writer().write_all(&[0]).unwrap();
 
     // Height.
-    stream.write_all(&0u32.to_le_bytes()).await.unwrap();
+    version
+        .payload_writer()
+        .write_all(&0u32.to_le_bytes())
+        .unwrap();
+
+    version.write(&mut stream).await;
 
     let ans = stream.read_u8().await.unwrap();
     println!("{}", ans);
