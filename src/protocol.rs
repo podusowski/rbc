e5facb1c42c82b4ae4a06fd1f730e0352894abb7
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-02-11 18:30:49 +0100

    removed old header building code

--- src/protocol.rs
+++ src/protocol.rs
@@ -8,7 +8,6 @@ use tokio::io::{AsyncWrite, AsyncWriteExt};
 /// and some intermediate form is needed.
 pub(crate) struct BitcoinMessage {
     new_header: BitcoinHeader,
-    header: [u8; 4 + 12 + 4 + 4],
     // TODO: Optimize this!
     payload: Vec<u8>,
 }
@@ -73,7 +72,7 @@ impl BitconSerializable for u32 {
 
 impl BitcoinMessage {
     pub fn new() -> Self {
-        let mut message = Self {
+        Self {
             new_header: BitcoinHeader {
                 magic: Default::default(),
                 command: Command {
@@ -82,37 +81,8 @@ impl BitcoinMessage {
                 payload_length: 0,
                 payload_hash: 0,
             },
-            header: Default::default(),
             payload: Vec::new(),
-        };
-
-        let mut header = message.header.as_mut_slice();
-
-        // https://developer.bitcoin.org/reference/p2p_networking.html#message-headers
-
-        // Magic start string. Common for all messages.
-        header.write_all(&0xf9beb4d9u32.to_be_bytes()).unwrap();
-
-        // Command, or type of the message.
-        crate::utils::write_padded_bytes(&mut header, b"version", 12);
-
-        // Length of the payload that follows the header. Set to zero, can be
-        // changed later.
-        header.write_all(&0u32.to_le_bytes()).unwrap();
-
-        // Checksum of the payload. Set to the empty string, can be changed
-        // later.
-        header.write_all(&0x5df6e0e2u32.to_le_bytes()).unwrap();
-
-        message
-    }
-
-    fn content_length(&mut self) -> &mut [u8] {
-        &mut self.header[16..20]
-    }
-
-    fn payload_hash(&mut self) -> &mut [u8] {
-        &mut self.header[20..24]
+        }
     }
 
     fn calculate_payload_hash(&self) -> [u8; 4] {
@@ -134,13 +104,6 @@ impl BitcoinMessage {
         // Finalize the message.
         self.new_header.payload_length = self.payload.len() as u32;
         self.new_header.payload_hash = u32::from_le_bytes(self.calculate_payload_hash());
-
-        let payload_len = self.payload.len() as u32;
-        self.content_length()
-            .copy_from_slice(&payload_len.to_le_bytes());
-
-        let hash = self.calculate_payload_hash();
-        self.payload_hash().copy_from_slice(&hash);
 
         let mut encoded = Vec::new();
         self.new_header.write_to(&mut encoded).unwrap();
